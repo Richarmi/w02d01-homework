b19a3b5de971e0c51b942351ac6741309f9830bb
Michael Richards <michaelrichards@Michaels-MacBook-Pro.local>
2018-06-18 16:59:56 -0600

    1st commit - selecting datatypes

---
+++ js/answers.js
@@ -0,0 +1,10 @@
+// 1. a boolean variable.
+// 2. a string
+// 3. an object containing strings for
+// 4. representing the hull, laser blasers,
+// tractor beam, and the warp drive.
+// 5. an array of objects, where each object contains a string for the student name and that student's location.
+// 6. an array of objects each containing
+// the corresponding name, location, and
+// array containing that student's favorite
+// TV shows.
