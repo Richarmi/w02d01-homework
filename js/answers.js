1b0b54a1e37e0520724bbc0d9e4a81a421dfd7da
Michael Richards <michaelrichards@Michaels-MacBook-Pro.local>
2018-06-18 17:16:55 -0600

    2nd commit - created some data structures and accessed some data

--- js/answers.js
+++ js/answers.js
@@ -8,3 +8,16 @@
 // the corresponding name, location, and
 // array containing that student's favorite
 // TV shows.
+
+let rainbow = [red, blue, green, yellow, orange, white, black, purple, pink, grey];
+
+let thisBlue = rainbow[1];
+
+let Michael = {
+  favoriteTown: "Westminster"
+  favoriteFood: "Pizza",
+  favoriteDataStructure: "Object",
+  favoriteHobby: "Fencing"
+}
+
+console.log(Michael.favoriteHobby);
